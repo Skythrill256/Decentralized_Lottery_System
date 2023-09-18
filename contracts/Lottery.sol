11299cae4d2286eabfb37da1c85d730511903492
Anjishnu Ganguly <113181374+Skythrill256@users.noreply.github.com>
2023-09-18 20:57:53 +0530

    Update Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,7 +8,7 @@
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.19;
 
 error lottery__notenoughETHEntered();
 error lottery__TransferFailed();
